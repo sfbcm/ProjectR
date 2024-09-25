f82aab91954d1ef0d44f13f79fdf123dd2795263
sfbcm <43795890+sfbcm@users.noreply.github.com>
2024-09-25 03:27:04 -0700

    Create getkey.js

---
+++ getkey.js
@@ -0,0 +1,18 @@
+//----------------------------------------------------------------------
+// (c) Copyright 2018-2024 Open Text or one of its affiliates.
+// The only warranties for products and services of Open Text and its affiliates and licensors ("Open Text") are as may be set forth in the express warranty statements accompanying such products and services. Nothing herein should be construed as constituting an additional warranty. Open Text shall not be liable for technical or editorial errors or omissions contained herein. The information contained herein is subject to change without notice.
+// Except as specifically indicated otherwise, this document contains confidential information and a valid license is required for possession, use or copying. If this work is provided to the U.S. Government, consistent with FAR 12.211 and 12.212, Commercial Computer Software, Computer Software Documentation, and Technical Data for Commercial Items are licensed to the U.S. Government under vendor's standard commercial license.
+//----------------------------------------------------------------------
+
+// PIE version: 1.2.1
+
+var PIE = {};  // PIE namespace
+
+// dynamically-generated PIE settings
+PIE.L = 6;
+PIE.E = 4;
+PIE.K = "5B2EC6E4DF056405A722CC37ACD29EA9";
+PIE.key_id = "17da8bac";
+PIE.phase = 0;
+
+module.exports = PIE;
